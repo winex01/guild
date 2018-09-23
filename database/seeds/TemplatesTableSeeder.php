7c218d61e0b71a5ff4ab4b75e98999f34113d3d8
winex01 <winnie131212592@gmail.com>
2018-09-23 16:21:51 +0800

    add template migration and seeders

---
+++ database/seeds/TemplatesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class TemplatesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('templates')->insert([
+            'description' => 'administrator',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'Winnie Damayo',
+            'email' => 'winnie131212592@gmail.com',
+            'password' => bcrypt('password123'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
