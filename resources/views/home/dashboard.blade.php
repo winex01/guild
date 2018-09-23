24e07aa73b87e340f0ebf47ff96fe4968a8e1a63
winex01 <winnie131212592@gmail.com>
2018-09-23 12:49:52 +0800

    WIP: template

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -195,7 +195,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.4.0
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2018-2019 <a href="#">{{ env('APP_NAME') }}</a>.</strong> All rights
     reserved.
   </footer>
 
